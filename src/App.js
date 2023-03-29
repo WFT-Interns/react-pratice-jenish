daa6ee1dc2a2665c0226c5e927630c14f0f0e144
Jenish Rijal <zenishrz@gmail.com>
2023-03-29 12:15:38 +0545

    added path to APIdata

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/login';
+import APIdata from './Pages/getData';
 
 function App() {
   return (
-      < Login/>
-    </div>
+    <>
+      {/* < Login/> */}
+      < APIdata/>
+    </>
   );
 }
 
