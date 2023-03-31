d49bc9029a99f02624811dbdddf4ffc4d76af82c
Jenish Rijal <zenishrz@gmail.com>
2023-03-31 15:03:35 +0545

    removed unused code

--- src/Components/api-table.jsx
+++ src/Components/api-table.jsx
@@ -2,48 +2,54 @@ import React, { useEffect, useState } from "react"
 import { DataGrid } from "@mui/x-data-grid";
 import axiosInstance from "./axiosInstance";
 import axios from "axios";
+import { Container } from "@mui/material";
+import makeStyles from "@mui/material";
 
-const StarWar = () => {
+const columns = [
+    { field: 'name', headerName: 'Name', width: 200 },
+    { field: 'diameter', headerName: 'Diameter', width: 200},
+    { field: 'climate', headerName: 'Climate', width: 200 },
+    { field: 'terrain', headerName: 'Terrain', width: 300 },
+]
+
+
+const StarWarTable = () => {
     const [planets, setPlanets] = useState([]);
 
-    const columns =  [
-        {field: 'name', headerName: 'Name', width: 200},
-        {field: 'diameter', headerName: 'Diameter', width: 90},
-        {field: 'climate', headerName: 'Climate', width: 100},
-        {field: 'terrain', headerName: 'Terrain', width: 100},
-    ]
-
-    useEffect (() => {
-        axiosInstance.get('planets/').then(response =>{
+    useEffect(() => {
+        axiosInstance.get('planets/').then(response => {
             const parsedData = response.data.results.map(sWar => ({
-                name:sWar.name,
+
+                name: sWar.name,
                 diameter: sWar.diameter,
                 climate: sWar.climate,
                 terrain: sWar.terrain,
             }));
             setPlanets(parsedData);
         })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .catch(function (error) {
+                console.log(error);
+            });
     }, []);
-    
+
     return (
         <>
-            {planets &&
-                <div style={{ height: 400, width: '100%' }}>
-                    <DataGrid
-                        columns={columns}
-                        rows={planets}
-                        pageSize={8}
-                        rowsPerPageOptions={[10]}
-                        disableSelectionOnClick />
-                </div>
-            }
-             
+            <Container>
+                {planets &&
+                    <div style={{ height: 700, width: '100%' }}>
+                        <DataGrid
+                            getRowId={(row) => row.name}
+                            columns={columns}
+                            rows={planets}
+                            rowsPerPageOptions={[5]}
+                            disableSelectionOnClick
+                            />
+                    </div>
+                }
+            </Container>
         </>
     )
 
 }
 
-export default StarWar;
+export default StarWarTable;
